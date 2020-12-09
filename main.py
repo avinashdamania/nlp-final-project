18d8be53f9753557b15151d4959467a8e3b8a360
Avinash Damania <avinash.damania@utexas.edu>
2020-12-08 18:00:00 -0600

    took out code to print probabilities

--- main.py
+++ main.py
@@ -476,10 +476,10 @@ def write_predictions(args, model, dataset, dataset_truecase):
 
                 heap = []
 
-                probs = []
-                for temp in topk:
-                    probs.append(temp[0])
-                print("PROBS:", probs)
+                # probs = []
+                # for temp in topk:
+                #     probs.append(temp[0])
+                # print("PROBS:", probs)
 
                 for max_prob, start_index, end_index in topk:
                     pred_span = str(passage[start_index:(end_index + 1)])
